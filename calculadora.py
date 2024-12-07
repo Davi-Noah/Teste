4fa382fe1d0e900ac038e22233540666d858a855
Davi-Noah <daviollee@gmail.com>
2024-12-07 19:29:55 -0300

    :bug: Começo calculadora

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+class Calculadora:
+    def __init__(self):
+        pass
+
+    def somar(self):
+        pass
+
+    def subtrair(self):
+        pass
+
+    def multiplicar(self):
+        pass
+
+    def dividir(self):
+        pass
